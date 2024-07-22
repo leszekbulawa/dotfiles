c5aa85c5ed9d13f94d498bcf7e8aceb79d53f1e0
Leszek Bulawa <leszekbu@gmail.com>
2024-07-22 19:54:27 +0200

    Install zsh; Conditionals for failing installations

--- install.sh
+++ install.sh
@@ -3,8 +3,19 @@
 set -e
 set -o pipefail
 
+# install zsh
+sudo apt install zsh
+
 # install ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [ ! -d $HOME/.oh-my-zsh ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+# install fzf
+if [ ! -d $HOME/.fzf ]; then
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install
+fi
 
 # install .zshrc config
 for config in .zshrc
